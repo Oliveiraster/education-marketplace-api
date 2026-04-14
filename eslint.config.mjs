eb671749010c0c7924b273b4df8823772c5d9125
Raphael Oliveira <oliveiraster.dev@gmail.com>
2026-04-14 11:46:49 -0300

    chore: initial project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  {
+    ignores: [
+      'eslint.config.mjs',
+      'dist',
+      'node_modules',
+      'test',
+      '*.config.js',
+    ],
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommendedTypeChecked,
+
+  eslintPluginPrettierRecommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+
+    plugins: {
+      import: importPlugin,
+    },
+
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+
+      'prettier/prettier': ['error', { endOfLine: 'lf' }],
+    },
+  },
+];
