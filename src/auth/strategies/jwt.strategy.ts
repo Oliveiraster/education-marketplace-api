7e13112fd747d4b7560ab9a675e864f6c791e2ec
Raphael Oliveira <oliveiraster.dev@gmail.com>
2026-04-17 03:24:56 -0300

    refactor(auth): support multiple roles and improve env config for multi-database

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -24,7 +24,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return {
       userId: payload.sub,
       email: payload.email,
-      role: payload.role,
+      roles: payload.roles,
     };
   }
 }
